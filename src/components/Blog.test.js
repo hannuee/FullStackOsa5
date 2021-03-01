c79408a027509e19312a1932576a092073272219
Hannu Erälaukko <32358926+hannuee@users.noreply.github.com>
2021-03-01 13:54:07 +0200

    Tehtävä 5.14 blogilistan testit step2

--- src/components/Blog.test.js
+++ src/components/Blog.test.js
@@ -3,8 +3,8 @@ import '@testing-library/jest-dom/extend-expect'
 import { render, fireEvent } from '@testing-library/react'
 import Blog from './Blog'
 
-test('renders title and author but not others as default', () => {
-    
+describe('<Blog />', () => {
+
     const blog = {
         author: 'Sale',
         id: '603ba1b93883751c20607000',
@@ -13,25 +13,52 @@ test('renders title and author but not others as default', () => {
         url: 'pressa.fi',
         user: { username: 'SauNii', name: 'Sauli', id: '603ba1483883751c20606fff' }
     }
-
+    
     const user = {
         token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6IlNhdU5paSIsImlkIjoiNjAzYmExNDgzODgzNzUxYzIwNjA2ZmZmIiwiaWF0IjoxNjE0NTk1NjYzfQ.rcYbDeQCi5xvDOnBtPOqm9Yo35S-ucI62PbetcvET-s',
         username: 'SauNii',
         name: 'Sauli'
     }
 
-    const mockHandleUpdate = jest.fn()
-    const mockHandleDeletion = jest.fn()
+    test('renders title and author but not url and likes as default', () => {
     
-    const component = render(
-        <Blog blog={blog} handleUpdate={mockHandleUpdate} handleDeletion={mockHandleDeletion} loggedUser={user} />
-    )
+        const mockHandleUpdate = jest.fn()
+        const mockHandleDeletion = jest.fn()
+        
+        const component = render(
+            <Blog blog={blog} handleUpdate={mockHandleUpdate} handleDeletion={mockHandleDeletion} loggedUser={user} />
+        )
+        
+        const limitedDiv = component.container.querySelector('.limitedInfo')
+        expect(limitedDiv).not.toHaveStyle('display: none')
+        expect(limitedDiv).toHaveTextContent('Pressan Blogi')
+        expect(limitedDiv).toHaveTextContent('Sale')
     
-    const limitedDiv = component.container.querySelector('.limitedInfo')
-    expect(limitedDiv).not.toHaveStyle('display: none')
-    expect(limitedDiv).toHaveTextContent('Pressan Blogi')
-    expect(limitedDiv).toHaveTextContent('Sale')
+        const fullDiv = component.container.querySelector('.fullInfo')
+        expect(fullDiv).toHaveStyle('display: none')
+    })
 
-    const fullDiv = component.container.querySelector('.fullInfo')
-    expect(fullDiv).toHaveStyle('display: none')
+    test('renders also url and likes when the view button has been pushed', () => {
+    
+        const mockHandleUpdate = jest.fn()
+        const mockHandleDeletion = jest.fn()
+        
+        const component = render(
+            <Blog blog={blog} handleUpdate={mockHandleUpdate} handleDeletion={mockHandleDeletion} loggedUser={user} />
+        )
+        
+        const button = component.getByText('view')
+        fireEvent.click(button)
+        
+        const limitedDiv = component.container.querySelector('.limitedInfo')
+        expect(limitedDiv).toHaveStyle('display: none')
+    
+        const fullDiv = component.container.querySelector('.fullInfo')
+        expect(fullDiv).not.toHaveStyle('display: none')
+        expect(fullDiv).toHaveTextContent('Pressan Blogi')
+        expect(fullDiv).toHaveTextContent('Sale')
+        expect(fullDiv).toHaveTextContent('pressa.fi')
+        expect(fullDiv).toHaveTextContent('likes 10')
+    })
+
 })
