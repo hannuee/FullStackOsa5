568883c22a43f87cf46d933f2010836d9aaced0f
Hannu Erälaukko <32358926+hannuee@users.noreply.github.com>
2021-03-01 20:17:23 +0200

    Linttaus

--- cypress/integration/note_app.spec.js
+++ cypress/integration/note_app.spec.js
@@ -1,126 +1,126 @@
 describe('Blog app', function() {
-    
+
+  beforeEach(function() {
+    cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    cy.request('POST', 'http://localhost:3003/api/users', { username: 'SauNii', name: 'Sauli', password: 'passu' })
+    cy.visit('http://localhost:3000')
+  })
+
+  it('Login form is shown', function() {
+    cy.get('h2').contains('Log in to application')
+    cy.get('form').contains('username')
+    cy.get('form').contains('password')
+    cy.get('#username').should('be.visible')
+    cy.get('#password').should('be.visible')
+    cy.get('#login').contains('login').should('be.visible')
+  })
+
+  describe('Login',function() {
+
+    it('succeeds with correct credentials', function() {
+      cy.get('#username').type('SauNii')
+      cy.get('#password').type('passu')
+      cy.get('#login').click()
+
+      cy.contains('Login success')
+      cy.contains('Sauli logged in')
+      cy.contains('blogs')
+    })
+
+    it('fails with wrong credentials', function() {
+      cy.get('#username').type('Sauli')
+      cy.get('#password').type('passu')
+      cy.get('#login').click()
+
+      cy.contains('invalid username or password')
+      cy.contains('Log in to application')
+      cy.get('#username').should('be.visible')
+      cy.get('#password').should('be.visible')
+      cy.get('#login').contains('login').should('be.visible')
+    })
+  })
+
+  describe('When logged in', function() {
+
     beforeEach(function() {
-        cy.request('POST', 'http://localhost:3003/api/testing/reset')
-        cy.request('POST', 'http://localhost:3003/api/users', { username: 'SauNii', name: 'Sauli', password: 'passu' })
-        cy.visit('http://localhost:3000')
-    })
-  
-    it('Login form is shown', function() {
-        cy.get('h2').contains('Log in to application')
-        cy.get('form').contains('username')
-        cy.get('form').contains('password')
-        cy.get('#username').should('be.visible') 
-        cy.get('#password').should('be.visible')
-        cy.get('#login').contains('login').should('be.visible')
-    })
-
-    describe('Login',function() {
-        
-        it('succeeds with correct credentials', function() {
-            cy.get('#username').type('SauNii')
-            cy.get('#password').type('passu')
-            cy.get('#login').click()
-
-            cy.contains('Login success')
-            cy.contains('Sauli logged in')
-            cy.contains('blogs')
-        })
-    
-        it('fails with wrong credentials', function() {
-            cy.get('#username').type('Sauli')
-            cy.get('#password').type('passu')
-            cy.get('#login').click()
-
-            cy.contains('invalid username or password')
-            cy.contains('Log in to application')
-            cy.get('#username').should('be.visible') 
-            cy.get('#password').should('be.visible')
-            cy.get('#login').contains('login').should('be.visible')
+      cy.request('POST', 'http://localhost:3003/api/login', { username: 'SauNii', password: 'passu' })
+        .then(response => {
+          localStorage.setItem('loggedUser', JSON.stringify(response.body))
+          cy.visit('http://localhost:3000')
         })
     })
 
-    describe('When logged in', function() {
+    it('A blog can be created', function() {
+      cy.contains('new blog').click()
+      cy.get('#title').type('Pressan blogi')
+      cy.get('#author').type('Sale')
+      cy.get('#url').type('pressa.fi')
+      cy.get('#create').click()
 
-        beforeEach(function() {
-            cy.request('POST', 'http://localhost:3003/api/login', { username: 'SauNii', password: 'passu' })
-                .then(response => { 
-                    localStorage.setItem('loggedUser', JSON.stringify(response.body))
-                    cy.visit('http://localhost:3000')
-                })
-        })
-    
-        it('A blog can be created', function() {
-            cy.contains('new blog').click()
-            cy.get('#title').type('Pressan blogi')
-            cy.get('#author').type('Sale')
-            cy.get('#url').type('pressa.fi')
-            cy.get('#create').click()
+      cy.contains('a new blog Pressan blogi by Sale added')
+      cy.contains('Pressan blogi Sale')
+    })
 
-            cy.contains('a new blog Pressan blogi by Sale added')
-            cy.contains('Pressan blogi Sale')
-        })
+    it('A blog can be liked', function() {
+      // Add a blog with a lazy method:
+      cy.contains('new blog').click()
+      cy.get('#title').type('Pressan blogi')
+      cy.get('#author').type('Sale')
+      cy.get('#url').type('pressa.fi')
+      cy.get('#create').click()
 
-        it('A blog can be liked', function() {
-            // Add a blog with a lazy method:
-            cy.contains('new blog').click()
-            cy.get('#title').type('Pressan blogi')
-            cy.get('#author').type('Sale')
-            cy.get('#url').type('pressa.fi')
-            cy.get('#create').click()
+      cy.contains('view').click()
+      cy.contains('like').click()
 
-            cy.contains('view').click()
-            cy.contains('like').click()
+      cy.contains('likes 1')
+    })
 
-            cy.contains('likes 1')
-        })
+    it('A blog can be removed', function() {
+      // Add a blog with a lazy method:
+      cy.contains('new blog').click()
+      cy.get('#title').type('Pressan blogi')
+      cy.get('#author').type('Sale')
+      cy.get('#url').type('pressa.fi')
+      cy.get('#create').click()
 
-        it('A blog can be removed', function() {
-            // Add a blog with a lazy method:
-            cy.contains('new blog').click()
-            cy.get('#title').type('Pressan blogi')
-            cy.get('#author').type('Sale')
-            cy.get('#url').type('pressa.fi')
-            cy.get('#create').click()
+      cy.contains('view').click()
+      cy.contains('remove').click()
 
-            cy.contains('view').click()
-            cy.contains('remove').click()
+      cy.wait(6000)
+      cy.contains('Pressan blogi').should('not.exist')
+    })
 
-            cy.wait(6000)
-            cy.contains('Pressan blogi').should('not.exist')
-        })
+    it('Blogs are ordered according to likes', function() {
+      // Add a blog with a lazy method:
+      cy.contains('new blog').click()
+      cy.get('#title').type('Paaministerin blogi')
+      cy.get('#author').type('Sanna')
+      cy.get('#url').type('paaministeri.fi')
+      cy.get('#create').click()
 
-        it('Blogs are ordered according to likes', function() {
-            // Add a blog with a lazy method:
-            cy.contains('new blog').click()
-            cy.get('#title').type('Paaministerin blogi')
-            cy.get('#author').type('Sanna')
-            cy.get('#url').type('paaministeri.fi')
-            cy.get('#create').click()
+      // Add 2 likes for it:
+      cy.contains('view').click()
+      cy.contains('like').click()
+      cy.wait(6000)
+      cy.contains('like').click()
 
-            // Add 2 likes for it:
-            cy.contains('view').click()
-            cy.contains('like').click()
-            cy.wait(6000)
-            cy.contains('like').click()
+      cy.wait(6000)
 
-            cy.wait(6000)
+      // Add another blog with a lazy method:
+      cy.contains('new blog').click()
+      cy.get('#title').type('Pressan blogi')
+      cy.get('#author').type('Sale')
+      cy.get('#url').type('pressa.fi')
+      cy.get('#create').click()
 
-            // Add another blog with a lazy method:
-            cy.contains('new blog').click()
-            cy.get('#title').type('Pressan blogi')
-            cy.get('#author').type('Sale')
-            cy.get('#url').type('pressa.fi')
-            cy.get('#create').click()
+      // Add 1 like for it:
+      cy.wait(3000)
+      cy.get('.limitedInfo:last').contains('view').click()
+      cy.get('.fullInfo:last').contains('like').click()
 
-            // Add 1 like for it:
-            cy.wait(3000)
-            cy.get('.limitedInfo:last').contains('view').click()
-            cy.get('.fullInfo:last').contains('like').click()
-            
-            // Check the ordering of the blogs:
-            cy.get('.fullInfo:first').contains('Paaministerin blogi')
-            cy.get('.fullInfo:last').contains('Pressan blogi')
-        })
-      })
+      // Check the ordering of the blogs:
+      cy.get('.fullInfo:first').contains('Paaministerin blogi')
+      cy.get('.fullInfo:last').contains('Pressan blogi')
+    })
+  })
 })
--- src/components/NewBlogForm.test.js
+++ src/components/NewBlogForm.test.js
@@ -5,34 +5,34 @@ import NewBlogForm from './NewBlogForm'
 
 describe('<NewBlogForm />', () => {
 
-    test('Form sends the filled data for the call back function', () => {
-    
-        const mockHandleCreation = jest.fn()
-        
-        const component = render(
-            <NewBlogForm createBlog={mockHandleCreation} />
-        )
+  test('Form sends the filled data for the call back function', () => {
 
-        const title = component.container.querySelector('#title')
-        const author = component.container.querySelector('#author')
-        const url = component.container.querySelector('#url')
-        const form = component.container.querySelector('form')
+    const mockHandleCreation = jest.fn()
 
-        fireEvent.change(title, { 
-            target: { value: 'Uusi blogi' } 
-        })
-        fireEvent.change(author, { 
-            target: { value: 'Influensseri' } 
-        })
-        fireEvent.change(url, { 
-            target: { value: 'blogi.fi' } 
-        })
-        fireEvent.submit(form)
+    const component = render(
+      <NewBlogForm createBlog={mockHandleCreation} />
+    )
 
-        expect(mockHandleCreation.mock.calls).toHaveLength(1)
-        expect(mockHandleCreation.mock.calls[0][0].title).toBe('Uusi blogi')
-        expect(mockHandleCreation.mock.calls[0][0].author).toBe('Influensseri')
-        expect(mockHandleCreation.mock.calls[0][0].url).toBe('blogi.fi')
+    const title = component.container.querySelector('#title')
+    const author = component.container.querySelector('#author')
+    const url = component.container.querySelector('#url')
+    const form = component.container.querySelector('form')
+
+    fireEvent.change(title, {
+      target: { value: 'Uusi blogi' }
     })
+    fireEvent.change(author, {
+      target: { value: 'Influensseri' }
+    })
+    fireEvent.change(url, {
+      target: { value: 'blogi.fi' }
+    })
+    fireEvent.submit(form)
+
+    expect(mockHandleCreation.mock.calls).toHaveLength(1)
+    expect(mockHandleCreation.mock.calls[0][0].title).toBe('Uusi blogi')
+    expect(mockHandleCreation.mock.calls[0][0].author).toBe('Influensseri')
+    expect(mockHandleCreation.mock.calls[0][0].url).toBe('blogi.fi')
+  })
 
 })
