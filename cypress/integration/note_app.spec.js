4e9d5080e55e915d9441e5634d45120c7a4ec614
Hannu Erälaukko <32358926+hannuee@users.noreply.github.com>
2021-03-01 18:42:52 +0200

    Tehtävä 5.18 blogilistan end to end testit step2

--- cypress/integration/note_app.spec.js
+++ cypress/integration/note_app.spec.js
@@ -1,6 +1,8 @@
 describe('Blog app', function() {
+    
     beforeEach(function() {
         cy.request('POST', 'http://localhost:3003/api/testing/reset')
+        cy.request('POST', 'http://localhost:3003/api/users', { username: 'SauNii', name: 'Sauli', password: 'passu' })
         cy.visit('http://localhost:3000')
     })
   
@@ -12,4 +14,29 @@ describe('Blog app', function() {
         cy.get('#password').should('be.visible')
         cy.get('#login').contains('login').should('be.visible')
     })
+
+    describe('Login',function() {
+        
+        it('succeeds with correct credentials', function() {
+            cy.get('#username').type('SauNii')
+            cy.get('#password').type('passu')
+            cy.get('#login').click()
+
+            cy.contains('Login success')
+            cy.contains('Sauli logged in')
+            cy.contains('blogs')
+        })
+    
+        it('fails with wrong credentials', function() {
+            cy.get('#username').type('Sauli')
+            cy.get('#password').type('passu')
+            cy.get('#login').click()
+
+            cy.contains('invalid username or password')
+            cy.contains('Log in to application')
+            cy.get('#username').should('be.visible') 
+            cy.get('#password').should('be.visible')
+            cy.get('#login').contains('login').should('be.visible')
+        })
+      })
 })
